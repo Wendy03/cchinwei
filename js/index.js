af91ab00d568fdafb18efb5b02e6e6452e847bf6
Wendy03 <wendysu03@gmail.com>
2023-09-25 22:51:24 +0800

    feat: Course Page

--- js/index.js
+++ js/index.js
@@ -1,19 +1,3 @@
-function closeOffcanvas(targetId) {
-  var offcanvas = document.getElementById('offcanvasExample');
-  var targetElement = document.querySelector(targetId);
-
-  if (offcanvas && targetElement) {
-    var offcanvasInstance = bootstrap.Offcanvas.getInstance(offcanvas);
-
-    if (offcanvasInstance) {
-      offcanvasInstance.hide();
-      setTimeout(function () {
-        targetElement.scrollIntoView({ behavior: 'smooth' });
-      }, 300);
-    }
-  }
-}
-
 const contactForm = document.getElementById('contactForm');
 contactForm.addEventListener('submit',(e) =>{
   e.preventDefault();
